1c1f052d42cb26cda99acfd3c5b9b50011fe43c6
Ahmad Raza <ahmadrazashafi@gmail.com>
2021-01-21 01:40:09 +0500

    Fully responsive Project animated page

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ document.getElementById("hemberg").addEventListener("click",function(){
    
 });
 
+
+
+
                     
 const currentLocation = location.href;
 const menuItems = document.querySelectorAll("a");
@@ -33,13 +36,48 @@ for (let i = 0; i < menuLength ; i++){
 }
 
 
- var totalproject  = 1; 
- 
- document.addEventListener("click",project1);
- var project1 = setInterval(counter1 , 15);
+
+var project1 = setInterval(counter1 , 140);
+var project2 = setInterval(counter2 , 140);
+var project3 = setInterval(counter3 , 140);
+ var totalProject1  = 1; 
+ var totalProject2  = 1; 
+ var totalProject3 = 1; 
 
   function counter1(){
-      totalProject++;
-      document.querySelector("#total").innerHTML = totalProject;
-      totalProject;
+
+      totalProject1++;
+      document.querySelector("#total").innerText = totalProject1;
+      
+
+      if(totalProject1==30){
+        clearInterval(project1);
+      }
+  
   }
+   
+  function counter2(){
+
+    totalProject2++;
+    document.querySelector("#total2").innerText = totalProject2;
+    
+
+    if(totalProject2==20){
+      clearInterval(project2);
+    }
+
+}
+
+
+function counter3(){
+
+    totalProject3++;
+    document.querySelector("#total3").innerText = totalProject3;
+    
+
+    if(totalProject3==65){
+      clearInterval(project3);
+    }
+
+}
+
